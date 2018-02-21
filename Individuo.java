1f01ecfef0e3ea8f36c191a6c0f90ad2517b543a
Barca88 <marcomatiaspg@gmail.com>
2018-02-21 21:45:04 +0000

    Individuo

--- Individuo.java
+++ Individuo.java
@@ -1,15 +1,17 @@
 package FaturasIRS;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class Individuo extends Contribuinte implements Serializable{
     // Var. de instancia
-
-    private int qDepAgre; //quantidade de depententes
+    private int qDepAgre; //quantidade de depententes no Agregado Familiar
     private List<int> lContAgre;//lista de n contribuinte do agregado
-    private double coeficiente;//todo coeficente fiscal
-    private List<Atividade> descontos; //todo códigos das atividades economicas que este individuo tem possibilidade de deduzir
+    private double coeficiente;//coeficente fiscal
+    private List<Atividade> descontos; //códigos das atividades economicas que este individuo tem possibilidade de deduzir
 
     //Construtor
     public Individuo(int nif){
@@ -18,7 +20,6 @@ public class Individuo extends Contribuinte implements Serializable{
         this.lContAgre = new ArrayList<int>();
         this.coeficiente = 0.0;
         this.descontos = new ArrayList<Atividade>();
-        }
     }
     public Individuo(int nif, String email, String nome, String morada,
                      String pwd, int qDepAgre, List<int> lContAgre,
@@ -35,6 +36,52 @@ public class Individuo extends Contribuinte implements Serializable{
     }
 
     //Getters
-    //TODO
+    public int getqDepAgre() {
+        return qDepAgre;
+    }
+    public List<int> getlContAgre(){
+        return this.lContAgre.stream().collect(Collectors.toCollection(ArrayList::new));
+    }
+    public double getCoeficiente() {
+        return coeficiente;
+    }
+    public List<Atividade> getDescontos() {
+        return this.descontos.stream().collect(Collectors.toCollection(ArrayList::new));
+    }
 
+    //Setters
+    public void setqDepAgre(int qDepAgre) {
+        this.qDepAgre = qDepAgre;
+    }
+    public void setlContAgre(List<int> lContAgre) {
+        this.lContAgre.stream().collect(Collectors.toCollection(ArrayList::new));
+    }
+    public void setCoeficiente(double coeficiente) {
+        this.coeficiente = coeficiente;
+    }
+    public void setDescontos(List<Atividade> descontos) {
+        this.descontos.stream().collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    //Metodos TODO
+
+
+    public Individuo clone(){
+        return new Individuo (this);
+    }
+    public boolean equals (Object obj) { // Todo porque é que nao se compara as listas?
+        if (obj == this)
+            return true;
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        Individuo o = (Individuo) obj;
+        return  o.getqDepAgre() == this.getqDepAgre() &&
+                o.getCoeficiente() == this.getCoeficiente();
+    }
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Quantidade de Dependentes no Agregado Familiar: ").append(qDepAgre).append("\n");
+        sb.append("Coeficente Fiscal").append(coeficiente).append("\n");
+        return sb.toString();
+    }
 }
